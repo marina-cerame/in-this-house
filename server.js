928de81b3d843b3c187f691a594691accb52f871
Marina Cerame <marina.cerame@gmail.com>
2018-03-22 23:14:26 -0500

    built the stuff. need to clean but whateve.

---
+++ server.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'pug')
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/andappreciate/:houseText', (req, res) => {
+  try {
+    res.render('index', {"houseText": req.params.houseText});
+    // res.sendFile('index.html', { root: __dirname + "/public" });
+  } catch(error) {
+    console.log(error);
+    res.statusCode = 501;
+    res.end('Whoops! We have a problem.');
+  }
+
+});
+
+app.get('/andappreciate', (req, res) => {
+  res.render('index', {"houseText": "[Your Text Here]"});
+});
+
+app.get('/:houseText', (req, res) => {
+  try {
+    res.render('index', {"houseText": req.params.houseText});
+    // res.sendFile('index.html', { root: __dirname + "/public" });
+  } catch(error) {
+    console.log(error);
+    res.statusCode = 501;
+    res.end('Whoops! We have a problem.');
+  }
+
+});
+
+app.all('*', (req, res) => {
+  res.render('index', {"houseText": "[Your Text Here]"});
+});
